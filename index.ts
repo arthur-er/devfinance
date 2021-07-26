3da263b13c8317de2ba05f56ad69af0e6f32a777
Arthur Emanuel <arthur-er@pm.me>
2021-07-26 17:40:06 -0300

    listen on env port

--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Servidor rodando na porta 3000");
 });
